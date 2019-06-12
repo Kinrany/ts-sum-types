9319b14564fc01842930a3c0b0f68b19b316d261
Kinrany <kinrany@yandex.com>
2019-06-13 01:13:46 +0300

    Fix SumTypeVariant

--- src/valueFactory.ts
+++ src/valueFactory.ts
@@ -12,6 +12,6 @@ export function id<T1, T2, T3>(): ValueFactory<[T1, T2, T3], [T1, T2, T3]>;
 export function id<T1, T2, T3, T4>(): ValueFactory<[T1, T2, T3, T4], [T1, T2, T3, T4]>;
 export function id<T1, T2, T3, T4, T5>(): ValueFactory<[T1, T2, T3, T4, T5], [T1, T2, T3, T4, T5]>;
 export function id<T1, T2, T3, T4, T5, T6>(): ValueFactory<[T1, T2, T3, T4, T5, T6], [T1, T2, T3, T4, T5, T6]>;
-export function id<T extends unknown[] = never[]>(): ValueFactory<T, T> {
+export function id<T extends unknown[]>(): ValueFactory<T, T> {
   return (...args) => args;
 }
